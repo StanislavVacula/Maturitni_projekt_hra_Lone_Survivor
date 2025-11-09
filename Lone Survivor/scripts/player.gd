413b3c9916f313ee4e4222b2e2f69106c2628a33
Stanislav Vacula <zobmik@seznam.cz>
2025-11-09 14:37:31 +0100

    Úpráva scriptu hlavní postavy - přidání animací

--- Lone Survivor/scripts/player.gd
+++ Lone Survivor/scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 130
 const JUMP_VELOCITY = -300
 
+@onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +13,22 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("jump")  # 🎬 přehraj animaci skoku
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		anim.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	# Animace podle stavu
+	if not is_on_floor():
+		anim.play("jump")
+	elif direction:
+		anim.play("walk")
+	else:
+		anim.play("idle")
