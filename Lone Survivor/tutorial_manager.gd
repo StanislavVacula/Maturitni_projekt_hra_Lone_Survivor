a0f37b5bc5010d93a784c83bb2541a33a09b44cb
Stanislav Vacula <zobmik@seznam.cz>
2026-01-25 14:25:09 +0100

    Tvorba tutorial levelu, přidání fontu

---
+++ Lone Survivor/tutorial_manager.gd
@@ -0,0 +1,58 @@
+extends Node
+
+enum TutorialStep {
+	MOVE,
+	JUMP,
+	DONE
+}
+
+var step := TutorialStep.MOVE
+var player: CharacterBody2D
+
+@onready var label: Label = $"../CanvasLayer/TutorialUI/TutorialText"
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+
+	if player == null:
+		push_error("TutorialManager: Player nebyl nalezen!")
+		return
+
+	if label == null:
+		push_error("TutorialManager: TutorialText nebyl nalezen!")
+		return
+
+	get_tree().paused = true
+	update_text()
+
+func _process(delta):
+	match step:
+		TutorialStep.MOVE:
+			check_move()
+		TutorialStep.JUMP:
+			check_jump()
+
+func update_text():
+	match step:
+		TutorialStep.MOVE:
+			label.text = "Použij ← → pro pohyb"
+		TutorialStep.JUMP:
+			label.text = "Stiskni SPACE pro skok"
+		TutorialStep.DONE:
+			label.text = "Super! Jsi připraven 👍"
+
+func check_move():
+	# když se hráč pohne doleva nebo doprava
+	if abs(player.velocity.x) > 0:
+		step = TutorialStep.JUMP
+		update_text()
+
+func check_jump():
+	# když hráč skočí
+	if not player.is_on_floor():
+		step = TutorialStep.DONE
+		update_text()
+
+		await get_tree().create_timer(1.0).timeout
+		get_tree().paused = false
+		label.visible = false
