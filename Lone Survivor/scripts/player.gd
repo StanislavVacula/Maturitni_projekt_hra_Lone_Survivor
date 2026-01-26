306e571ef65c48a25a559554808da2057c5d234b
Stanislav Vacula <zobmik@seznam.cz>
2026-01-26 18:02:24 +0100

    Úprava tutorialu, změna pozadí

--- Lone Survivor/scripts/player.gd
+++ Lone Survivor/scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const SPEED = 80
 const JUMP_VELOCITY = -150
 
+var input_enabled := true
+
 @onready var anim = $AnimatedSprite2D
 @onready var jumpSound = $JumpSound
 
@@ -12,13 +14,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Skok
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if input_enabled and Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("jump")
 		jumpSound.play()
 
 	# Pohyb vodorovně
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := 0.0
+	if input_enabled:
+		direction = Input.get_axis("ui_left", "ui_right")
+
 	velocity.x = direction * SPEED
 
 	# Otočení animace podle směru pohybu
--- Lone Survivor/tutorial_manager.gd
+++ Lone Survivor/tutorial_manager.gd
@@ -18,11 +18,6 @@ func _ready():
 		push_error("TutorialManager: Player nebyl nalezen!")
 		return
 
-	if label == null:
-		push_error("TutorialManager: TutorialText nebyl nalezen!")
-		return
-
-	get_tree().paused = true
 	update_text()
 
 func _process(delta):
@@ -35,6 +30,7 @@ func _process(delta):
 func update_text():
 	match step:
 		TutorialStep.MOVE:
+			label.visible = true
 			label.text = "Použij ← → pro pohyb"
 		TutorialStep.JUMP:
 			label.text = "Stiskni SPACE pro skok"
@@ -42,17 +38,15 @@ func update_text():
 			label.text = "Super! Jsi připraven 👍"
 
 func check_move():
-	# když se hráč pohne doleva nebo doprava
+	# hráč se skutečně pohybuje
 	if abs(player.velocity.x) > 0:
 		step = TutorialStep.JUMP
 		update_text()
 
 func check_jump():
-	# když hráč skočí
 	if not player.is_on_floor():
 		step = TutorialStep.DONE
 		update_text()
 
 		await get_tree().create_timer(1.0).timeout
-		get_tree().paused = false
 		label.visible = false
