32d7e77252d1be7695adbc1b8d32869555754235
Stanislav Vacula <zobmik@seznam.cz>
2026-02-02 16:46:46 +0100

    Building Tutorial

--- Lone Survivor/scripts/death_menu.gd
+++ Lone Survivor/scripts/death_menu.gd
@@ -1,23 +1,19 @@
 extends Control
 
 func _ready():
-	print("DEATH MENU READY")
 	hide()
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 
 func show_menu():
 	show()
 	get_tree().paused = true
 
 func _on_restart_button_pressed():
+	print("restart click")
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 func _on_menu_button_pressed():
+	print("menu clicked")
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-	
-func show_menu2():
-	print("Viewport:", get_viewport_rect().size)
-	print("Canvas transform:", get_canvas_transform())
-	show()
-	get_tree().paused = true
