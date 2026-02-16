b520f50dabae1877cb3b63b207ff1144bed9a9ab
Stanislav Vacula <zobmik@seznam.cz>
2026-02-16 14:14:32 +0100

    Main menu done

--- Lone Survivor/scripts/main_menu.gd
+++ Lone Survivor/scripts/main_menu.gd
@@ -1,7 +1,16 @@
 extends Control
 
+@onready var play_button = $VBoxContainer/PlayButton
+@onready var quit_button = $VBoxContainer/QuitButton
+
+@export var game_scene_path: String = "res://scenes/tutorial.tscn" 
+
+func _ready():
+	play_button.pressed.connect(_on_play_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/Level.tscn")
+	get_tree().change_scene_to_file(game_scene_path)
 
 func _on_quit_pressed():
 	get_tree().quit()
