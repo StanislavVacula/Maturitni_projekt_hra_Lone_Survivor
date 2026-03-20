8492846d7fcdb3f551f0574d9e3d5868d2f3260b
Stanislav Vacula <zobmik@seznam.cz>
2026-03-20 15:54:38 +0100

    Win_menu update

--- Lone Survivor/scripts/win_menu.gd
+++ Lone Survivor/scripts/win_menu.gd
@@ -1,12 +1,29 @@
 extends Control
 
+@onready var mission_completed_sound = $MissionCompleted
+@onready var anim_player = $AnimationPlayer
+
 func _ready():
-	visible = false
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	hide()
+	if anim_player:
+		anim_player.play("RESET")
 
 func show_win():
-	visible = true
-	get_tree().paused = true 
+	show()
+	get_tree().paused = true
+	
+	if anim_player and anim_player.has_animation("fade_in"):
+		anim_player.play("fade_in")
+	
+	if mission_completed_sound and mission_completed_sound.stream:
+		mission_completed_sound.play()
 
-func _on_next_level_pressed():
-	get_tree().paused = false 
-	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_next_mission_button_pressed() -> void:
+	get_tree().paused = false
+	
+	var err = get_tree().change_scene_to_file("res://scenes/mission_2.tscn")
