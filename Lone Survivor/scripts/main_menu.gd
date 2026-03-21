317c0ab91babf196110a4b61c13021da543ca3ed
Stanislav Vacula <zobmik@seznam.cz>
2026-03-21 17:42:02 +0100

    controls done

--- Lone Survivor/scripts/main_menu.gd
+++ Lone Survivor/scripts/main_menu.gd
@@ -2,22 +2,20 @@ extends Control
 
 @onready var play_button = $VBoxContainer/PlayButton
 @onready var quit_button = $VBoxContainer/QuitButton
-@onready var options_panel = $OptionsPanel
+@onready var controls_button = $VBoxContainer/ControlsButton
 
 @export var game_scene_path: String = "res://scenes/tutorial.tscn" 
 
 func _ready():
 	play_button.pressed.connect(_on_play_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
+	controls_button.pressed.connect(_on_controls_button_pressed)
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file(game_scene_path)
 
 func _on_quit_pressed():
 	get_tree().quit()
-	
-func _on_options_button_pressed():
-	options_panel.show()
 
-func _on_back_button_pressed():
-	options_panel.hide()
+func _on_controls_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/controls.tscn")
